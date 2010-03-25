7ebdfa71bedcae898fa5f57bcbc51ce4b9b2a1c7
Manuel Zeise <zeise@cern.ch>
2010-03-25 21:34:48 +0100

    MET/sumEt cut added

--- Cuts/EventSelectionCuts.cc
+++ Cuts/EventSelectionCuts.cc
@@ -1,9 +1,13 @@
 #include "GoodTracksFractionCut.h"
 #include "L1TriggerCut.h"
+#include "METoverSumEtCut.h"
+
+#include "DataFormats/interface/KBasic.h" // to get KDataMET
 
 int main()
 {
 	KappaTools::GoodTracksFractionCut GoodTracksFractionCut_Test;
 	KappaTools::L1TriggerCut L1TriggerCut_Test;
+	KappaTools::METoverSumEtCut<KDataMET> METoverSumEtCut_Test;
 	return 0;
 }
