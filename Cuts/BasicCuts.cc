c4de19284267617f1bdc39513140b6cd1ccbb8ca
Manuel Zeise <zeise@cern.ch>
2010-03-25 21:27:46 +0100

    re-arranged compile test files for cut classes

--- Cuts/BasicCuts.cc
+++ Cuts/BasicCuts.cc
@@ -7,5 +7,18 @@
 #include "InvariantMassCut.h"
 #include "DeltaRCut.h"
 #include "ChargeCut.h"
-#include "GoodTracksFractionCut.h"
-#include "L1TriggerCut.h"
+
+#include "DataFormats/interface/KBasic.h" // to get KDataTrack
+
+int main()
+{
+	KappaTools::NoneCut NoneCut_Test;
+	KappaTools::ExistenceCut<KDataTrack> ExistenceCut_Test;
+	KappaTools::PtCut<KDataTrack> PtCut_Test;
+	KappaTools::EtaCut<KDataTrack> EtaCut_Test;
+	KappaTools::InvariantMassCut<KDataTrack> InvariantMassCut_Test;
+	KappaTools::DeltaRCut<KDataTrack, KDataTrack> DeltaRCut_Test;
+	KappaTools::ChargeCut<KDataTrack> ChargeCut_Test;
+
+	return 0;
+}
---
+++ Cuts/EventSelectionCuts.cc
@@ -0,0 +1,9 @@
+#include "GoodTracksFractionCut.h"
+#include "L1TriggerCut.h"
+
+int main()
+{
+	KappaTools::GoodTracksFractionCut GoodTracksFractionCut_Test;
+	KappaTools::L1TriggerCut L1TriggerCut_Test;
+	return 0;
+}
