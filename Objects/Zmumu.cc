0ad56b36a04d28aff6168951cdf4d2afad8e5eab
Manuel Zeise <zeise@cern.ch>
2010-03-27 00:09:19 +0100

    object for Z->mumu added (recoiling jet, MET and trigger missing)

---
+++ Objects/Zmumu.cc
@@ -0,0 +1,108 @@
+#include "Zmumu.h"
+
+template <typename JetType>
+RMLV KappaTools::ZmumuObjects<JetType>::getDiMuP4()
+{
+	if (!muon1 || !muon2)
+		return RMLV();
+	return (muon1->p4+muon2->p4);
+}
+
+template <typename JetType>
+void KappaTools::ZmumuObjects<JetType>::printInformation()
+{
+	RMLV z = getDiMuP4();
+	std::cout << "di-muon candidate:\n";
+	std::cout << "\t mass: " << z.mass() << "\n";
+	std::cout << "\t pT = " << z.pt() << "\t";
+	std::cout << "\t eta = " << z.eta() << "\t";
+	std::cout << "\t phi = " << z.phi() << "\n";
+	std::cout << "\n";
+	std::cout << "\t Muon 1: " << muon1->p4 << "\n";
+	printMuonInformation(muon1);
+	std::cout << "\n";
+	std::cout << "\t Muon 2: " << muon2->p4 << "\n";
+	printMuonInformation(muon2);		
+}
+
+template <typename JetType>
+void KappaTools::ZmumuObjects<JetType>::printMuonInformation(KDataMuon * muon)
+{
+	std::cout << "\t\t #chambers:    " << muon->numberOfChambers << "\n";
+	std::cout << "\t\t #track hits:  " << muon->trackHits << "\n";
+	std::cout << "\t\t vertex:       " << muon->vertex.position << "\n";												
+	std::cout << "\t\t ecal iso 03:  " << muon->ecalIso03 << "\n";
+	std::cout << "\t\t hcal iso 03:  " << muon->hcalIso03 << "\n";
+	std::cout << "\t\t track iso 03: " << muon->trackIso03 << "\n";
+}
+
+template <typename JetType>
+void KappaTools::ZmumuObjects<JetType>::recalcP4()
+{
+	if (!muon1 || !muon2)
+		p4 = RMLV();
+	else
+		p4 = muon1->p4 + muon2->p4;
+}
+
+template <typename JetType>
+void KappaTools::ZmumuObjects<JetType>::setMuons(KDataMuon * muon1_, KDataMuon * muon2_)
+{
+	muon1 = muon1_;
+	muon2 = muon2_;
+	recalcP4();
+}
+
+/*
+template <typename JetType>
+void KappaTools::ZmumuObjects<JetType>::setRJet(JetType * rjet_)
+{
+	rjet = rjet_;
+}
+
+template <typename JetType>
+void KappaTools::ZmumuObjects<JetType>::setMET(MTAPFMET * met_)
+{
+	met = met_;
+}
+
+template <typename JetType>
+void KappaTools::ZmumuObjects<JetType>::setTrigger(MTATrigger * trigger_)
+{
+	trigger = trigger_;
+}
+*/
+
+template <typename JetType>
+void KappaTools::ZmumuObjects<JetType>::setPV(KDataVertex * primaryvertex_)
+{
+	primaryvertex = primaryvertex_;
+}
+
+template <typename JetType>
+KDataMuon * KappaTools::ZmumuObjects<JetType>::getMuon1()
+{
+	return muon1;
+}
+
+template <typename JetType>
+KDataMuon * KappaTools::ZmumuObjects<JetType>::getMuon2()
+{
+	return muon2;
+}
+
+/*
+template <typename JetType>
+JetType * KappaTools::ZmumuObjects<JetType>::getRJet()
+{
+	return rjet;
+}
+*/
+
+template <typename JetType>
+KDataVertex * KappaTools::ZmumuObjects<JetType>::getPV()
+{
+	return primaryvertex;
+}
+
+//template class KappaTools::ZmumuObjects<MTAPFJet>;
---
+++ Objects/Zmumu.h
@@ -0,0 +1,44 @@
+#ifndef KZmumuObjects_h
+#define KZmumuObjects_h
+
+#include "DataFormats/interface/KMuon.h"
+#include "DataFormats/interface/KBasic.h"
+#include "DataFormats/interface/KLorentzVector.h"
+#include <stdlib.h>
+
+namespace KappaTools
+{
+	template <typename JetType>
+	class ZmumuObjects
+	{
+		private:
+			void printMuonInformation(KDataMuon * muon);
+			KDataMuon * muon1, * muon2;
+			//JetType * rjet;	// recoil jet
+			//MTAPFMET * met;
+			//MTATrigger * trigger;
+			KDataVertex * primaryvertex;
+			void recalcP4();
+		public:
+			void printInformation();
+			RMLV p4;
+			RMLV getDiMuP4();
+			RMLV getDiMu() { return getDiMuP4(); };
+			
+			void setMuons(KDataMuon * muon1, KDataMuon * muon2);
+			//void setRJet(JetType * rjet);
+			//void setMET(MTAPFMET * met);
+			//void setTrigger(MTATrigger * trigger);
+			void setPV(KDataVertex * primaryvertex);
+			
+			KDataMuon * getMuon1();
+			KDataMuon * getMuon2();
+			//JetType * getRJet();
+			KDataVertex * getPV();
+			
+			//ZmumuObjects() : muon1(0), muon2(0), rjet(0), met(0), trigger(0), primaryvertex(0) {};
+			ZmumuObjects() : muon1(0), muon2(0), primaryvertex(0) {};
+	};
+}
+
+#endif
