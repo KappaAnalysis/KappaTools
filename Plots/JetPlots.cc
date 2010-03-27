bea72a8ea1f8737379ad82b9da14c47e445aa918
Manuel Zeise <zeise@cern.ch>
2010-03-27 10:18:49 +0100

    standard plots for jets added

---
+++ Plots/JetPlots.cc
@@ -0,0 +1,113 @@
+#include "JetPlots.h"
+
+namespace KappaTools
+{
+	template <typename JetType>
+	StandardJetPlots<JetType>::StandardJetPlots(TDirectory * tmpFile, TString tmpDirectory, TString tmpSubDirectory)
+	{
+		getDirectory(tmpFile, tmpDirectory, tmpSubDirectory);
+
+		jet_pt 					= new TH1D(("pt"),"p_{T} of muon", 75, 0., 150.);
+		jet_pt_low		 		= new TH1D(("pt_low"),"p_{T} of muon", 50, 0., 25.);
+		jet_pt_full		 		= new TH1D(("pt_full"),"p_{T} of muon", 100, 0., 1000.);
+		jet_eta 					= new TH1D(("eta"),"#eta of muon", 50, -5., 5.);
+		jet_eta_zoom			= new TH1D("eta_zoom","#eta of muon", 50, -2.5, 2.5);
+		jet_phi					= new TH1D(("phi"),"#phi of muon", 50, -3.5, 3.5);
+
+		charge = new TH1D("charge","charge of jet", 5, -2, 2);
+		n90 = new TH1D("n90","number of constituents carrying 0.9 of the energy", 50, 0, 50);
+		nConstituents = new TH1D("nConstituents","number of constituents", 50, 0, 50);
+		emFraction = new TH1D("emFraction", "electromagnetic fraction", 50, 0., 1.);
+		hadFraction = new TH1D("hadFraction", "hadronic fraction", 50, 0., 1.);
+
+		initSpecific();
+	}
+
+	template <typename JetType>
+	void StandardJetPlots<JetType>::process(JetType * jet, KDataVertex * pv, double weight)
+	{
+		if (!jet)
+			return;
+
+		jet_pt->Fill(jet->p4.pt(), weight);
+		jet_pt_low->Fill(jet->p4.pt(), weight);
+		jet_pt_full->Fill(jet->p4.pt(), weight);
+		jet_eta->Fill(jet->p4.eta(), weight);
+		jet_eta_zoom->Fill(jet->p4.eta(), weight);
+		jet_phi->Fill(jet->p4.phi(), weight);
+
+		charge->Fill(jet->charge, weight);
+		n90->Fill(jet->n90, weight);
+		nConstituents->Fill(jet->nConstituents, weight);
+		emFraction->Fill(jet->emFraction, weight);
+		hadFraction->Fill(jet->hadFraction, weight);
+
+		processSpecific(jet, pv, weight);
+	}
+	template <typename JetType>
+	void StandardJetPlots<JetType>::final()
+	{
+	}
+
+/*
+	template <>
+	void StandardJetPlots<MTAPFJet>::initSpecific()
+	{
+		neutralEmFraction = new TH1D("neutralEmFraction", "neutral electromagnetic fraction", 50, 0., 1.);
+		chargedEmFraction = new TH1D("chargedEmFraction", "charged electromagnetic fraction", 50, 0., 1.);
+		neutralHadFraction = new TH1D("neutralHadFraction", "neutral electromagnetic fraction", 50, 0., 1.);
+		chargedHadFraction = new TH1D("chargedHadFraction", "charged electromagnetic fraction", 50, 0., 1.);
+	}
+*/
+
+	template <typename JetType>
+	void StandardJetPlots<JetType>::initSpecific()
+	{
+	}
+
+/*
+	template <>
+	void StandardJetPlots<MTAPFJet>::processSpecific(MTAPFJet * jet, KDataVertex * pv, double weight)
+	{
+		if (!jet)
+			return;
+		neutralEmFraction->Fill(jet->neutralEmFraction, weight);
+		chargedEmFraction->Fill(jet->chargedEmFraction, weight);
+		neutralHadFraction->Fill(jet->neutralHadFraction, weight);
+		chargedHadFraction->Fill(jet->chargedHadFraction, weight);
+	}
+*/
+
+	template <typename JetType>
+	void StandardJetPlots<JetType>::processSpecific(JetType * jet, KDataVertex * pv, double weight)
+	{
+	}
+}
+
+//template class KappaTools::StandardJetPlots<MTAPFJet>;
+
+/*
+MTA::StandardPFJetsPlots::StandardPFJetsPlots(TDirectory * tmpFile, TString tmpDirectory, TString tmpSubDirectory) : StandardJetPlots(tmpFile, tmpDirectory, tmpSubDirectory)
+{
+	neutralEmFraction = new TH1D("neutralEmFraction", "neutral electromagnetic fraction", 50, 0., 1.);
+	chargedEmFraction = new TH1D("chargedEmFraction", "charged electromagnetic fraction", 50, 0., 1.);
+	neutralHadFraction = new TH1D("neutralHadFraction", "neutral electromagnetic fraction", 50, 0., 1.);
+	chargedHadFraction = new TH1D("chargedHadFraction", "charged electromagnetic fraction", 50, 0., 1.);
+}
+
+void MTA::StandardPFJetsPlots::process(MTAPFJet * jet, MTAVertex * pv)
+{
+	neutralEmFraction->Fill(jet->neutralEmFraction);
+	chargedEmFraction->Fill(jet->chargedEmFraction);
+	neutralHadFraction->Fill(jet->neutralHadFraction);
+	chargedHadFraction->Fill(jet->chargedHadFraction);
+
+	StandardJetPlots::process(jet, pv);
+}
+
+void MTA::StandardPFJetsPlots::final()
+{
+	StandardJetPlots::final();
+}
+*/
+
---
+++ Plots/JetPlots.h
@@ -0,0 +1,53 @@
+#ifndef KJetPlots_h
+#define KJetPlots_h
+
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TMath.h"
+#include "TDirectory.h"
+#include "BasePlot.h"
+#include "DataFormats/interface/KBasic.h"
+
+namespace KappaTools
+{
+	template <typename JetType>
+	class StandardJetPlots : public BasePlot
+	{
+		private:
+			TH1D * jet_pt, * jet_pt_low, * jet_pt_full;
+			TH1D * jet_eta, * jet_eta_zoom, * jet_phi;
+
+			TH1D * charge;
+			TH1D * n90;
+			TH1D * nConstituents;
+			TH1D * emFraction, * hadFraction;
+
+			// pfjet-specific:
+			TH1D * neutralEmFraction, * chargedEmFraction;
+			TH1D * neutralHadFraction, * chargedHadFraction;
+
+			// calojet-specific:
+
+			void initSpecific();
+			void processSpecific(JetType * jet, KDataVertex * pv, double weight);
+		public:
+			StandardJetPlots(TDirectory * tmpFile, TString tmpDirectory, TString tmpSubDirectory="");
+
+			// TDirectory
+			void process(JetType * jet, KDataVertex * pv = 0, double weight = 1.);
+			void final();
+	};
+	/*
+	class StandardPFJetsPlots : public StandardJetPlots<typename JetType>
+	{
+		private:
+
+		public:
+			StandardPFJetsPlots(TDirectory * tmpFile, TString tmpDirectory, TString tmpSubDirectory="");
+			void process(MTAPFJet * jet, MTAVertex * pv = 0);
+			void final();
+	};
+	*/
+}
+#endif
+
