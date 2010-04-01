9e65f242e4d8818306a8feedac6a352606171454
Manuel Zeise <zeise@cern.ch>
2010-04-01 11:29:42 +0200

    provider for KLumiMetadata added

---
+++ Tools/LumiMetadataProvider.cc
@@ -0,0 +1,28 @@
+#include "LumiMetadataProvider.h"
+
+KappaTools::LumiMetadataProvider::LumiMetadataProvider(TTree * lumis_tree)
+{
+	KLumiMetadata * m_eventinfo = new KLumiMetadata();
+	TBranch * b_eventinfo;
+	lumis_tree->SetBranchAddress("KLumiMetadata", &m_eventinfo, &b_eventinfo);
+	
+	Long64_t nentries = lumis_tree->GetEntries();
+
+	std::cout << nentries << " lumi blocks\n";
+
+	for (unsigned int i=0;i<nentries;i++)	
+	{
+		lumis_tree->GetEntry(i);
+		std::pair<unsigned long,unsigned long> idn (m_eventinfo->nRun, m_eventinfo->nLumi);
+
+		tmpLumiMetadatas[idn]=(KLumiMetadata)(*m_eventinfo);
+	}
+}
+
+KLumiMetadata * KappaTools::LumiMetadataProvider::get(KEventMetadata * eventMetadata)
+{
+	std::pair<unsigned long,unsigned long> idn (eventMetadata->nRun, eventMetadata->nLumi);
+	return &tmpLumiMetadatas[idn];
+}	
+
+
---
+++ Tools/LumiMetadataProvider.h
@@ -0,0 +1,22 @@
+#ifndef KLumiMetadataProvider_h
+#define KLumiMetadataProvider_h
+
+#include "DataFormats/interface/KMetadata.h"
+#include <map>
+#include <iostream>
+#include <string>
+#include "TTree.h"
+#include <algorithm>
+
+namespace KappaTools
+{
+	class LumiMetadataProvider
+	{
+		private:
+			std::map< std::pair<unsigned long,unsigned long> , KLumiMetadata > tmpLumiMetadatas;
+		public:
+			LumiMetadataProvider(TTree * lumis_tree);
+			KLumiMetadata * get(KEventMetadata * eventMetadata);
+	};
+}
+#endif
