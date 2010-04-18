7ec67395141deb73e4ce0b6a66a8818c6c2c8e3b
Manuel Zeise <zeise@cern.ch>
2010-04-18 13:01:45 +0200

    PlotsByPt and PlotsByEta added

--- Plots/StandardP4Plots.h
+++ Plots/StandardP4Plots.h
@@ -6,6 +6,8 @@
 #include "BasePlot.h"
 #include "DataFormats/interface/KLorentzVector.h"
 
+#include "MuonPlots.h"
+
 namespace KappaTools
 {
 	class StandardP4Plots : public BasePlot
@@ -21,6 +23,51 @@ namespace KappaTools
 
 			void process(RMLV p4, double weight = 1.0);
 			void final();
+	};
+
+	template <typename PlottingClass, typename ObjectType>
+	class PlotsByPt : public BasePlot
+	{
+		private:
+			PlottingClass * all, * underflow, * overflow;
+			std::vector<PlottingClass> plotsByBin;
+			std::vector<double> binning;
+			int mode;
+		public:
+			//PlotsByPt(TDirectory * tmpFile, TString tmpDirectory, TString tmpSubDirectory="");
+			PlotsByPt(std::vector<double> binning_, TDirectory * tmpFile, TString tmpDirectory, TString tmpSubDirectory="");
+			void process(ObjectType * obj, KDataVertex * pv = 0, double weight = 1.);
+			void final();
+
+			void setMode(char mode_);
+
+			static const char FILL_EXCL = 1;
+			static const char FILL_INCL_UP = 2;
+			static const char FILL_INCL_DOWN = 4;
+
+			static const char FILL_ABS = 8;
+	};
+
+	template <typename PlottingClass, typename ObjectType>
+	class PlotsByEta : public BasePlot
+	{
+		private:
+			PlottingClass * all, * underflow, * overflow;
+			std::vector<PlottingClass> plotsByBin;
+			std::vector<double> binning;
+			int mode;
+		public:
+			PlotsByEta(std::vector<double> binning_, TDirectory * tmpFile, TString tmpDirectory, TString tmpSubDirectory="");
+			void process(ObjectType * obj, KDataVertex * pv = 0, double weight = 1.);
+			void final();
+
+			void setMode(char mode_);
+
+			static const char FILL_EXCL = 1;
+			static const char FILL_INCL_UP = 2;
+			static const char FILL_INCL_DOWN = 4;
+
+			static const char FILL_ABS = 8;
 	};
 }
 #endif
