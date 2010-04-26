36e6d1464efae09f1f0246cdcc8af5021711b15b
Fred Stober <stober@cern.ch>
2010-04-26 17:31:35 +0200

    Added Jet ID helper

---
+++ RootTools/JetID.h
@@ -0,0 +1,23 @@
+#ifndef KAPPA_JETID_H
+#define KAPPA_JETID_H
+
+template<typename T>
+bool jetIDLoose(const T &jet)
+{
+	return true;
+}
+
+template<>
+bool jetIDLoose(const KDataJet &jet)
+{
+	if (
+		((fabs(jet.p4.eta()) > 2.6) || (jet.emf > 0.01)) &&		// Remove HCAL noise for |eta|
+		(jet.fHPD < 0.98) &&	// Remove defective HCAL cells
+		(jet.n90Hits > 1)	// Remove ECAL spikes
+		)
+		return true;
+	else
+		return false;
+}
+
+#endif
