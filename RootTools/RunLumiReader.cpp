dfad1d42f8cee66fd67e1213c8a14782046b6d71
Fred Stober <stober@cern.ch>
2010-04-27 19:08:51 +0200

    Added RunLumiReader

---
+++ RootTools/RunLumiReader.cpp
@@ -0,0 +1,33 @@
+#include "RunLumiReader.h"
+#include <boost/property_tree/json_parser.hpp>
+#include "Toolbox/StringTools.h"
+
+void readLumiFilter(const std::string json, std::map<run_id, std::vector<std::pair<lumi_id, lumi_id> > > &lumifilter)
+{
+	using boost::property_tree::ptree;
+	ptree pt;
+	read_json(json, pt);
+	for (ptree::const_iterator itRun = pt.begin(); itRun != pt.end(); ++itRun)
+	{
+		run_id run = parse<run_id>(itRun->first);
+		for (ptree::const_iterator itLumiList = itRun->second.begin(); itLumiList != itRun->second.end(); ++itLumiList)
+		{
+			lumi_id lumi_low = 0, lumi_high = 0;
+			for (ptree::const_iterator itLumiRange = itLumiList->second.begin(); itLumiRange != itLumiList->second.end(); ++itLumiRange)
+			{
+				if (lumi_low == 0)
+					lumi_low = parse<lumi_id>(itLumiRange->second.data());
+				else if (lumi_high == 0)
+					lumi_high = parse<lumi_id>(itLumiRange->second.data());
+			}
+			lumifilter[run].push_back(std::make_pair(lumi_low, lumi_high));
+		}
+	}
+}
+
+RunLumiSelector::RunLumiSelector(const run_id _passRun, const std::string json)
+	: passRun(_passRun)
+{
+	if (json != "")
+		readLumiFilter(json, lumifilter);
+}
---
+++ RootTools/RunLumiReader.h
@@ -0,0 +1,29 @@
+#include <DataFormats/src/classes.h>
+#include <boost/property_tree/ptree.hpp>
+
+void readLumiFilter(const std::string json, std::map<run_id, std::vector<std::pair<lumi_id, lumi_id> > > &lumifilter);
+
+class RunLumiSelector
+{
+public:
+	RunLumiSelector(const run_id _passRun = 1, const std::string json = "");
+	inline bool accept(const run_id run, const lumi_id lumi) const
+	{
+		if (run <= passRun)
+			return true;
+		std::map<run_id, std::vector<std::pair<lumi_id, lumi_id> > >::const_iterator itRun = lumifilter.find(run);
+		if (itRun == lumifilter.end())
+			return false;
+		const std::vector<std::pair<lumi_id, lumi_id> > &lumis = itRun->second;
+		for (std::vector<std::pair<lumi_id, lumi_id> >::const_iterator itLumis = lumis.begin(); itLumis != lumis.end(); ++itLumis)
+			if (lumi < itLumis->first)
+				return false;
+			else
+				if ((lumi >= itLumis->first) && (lumi <= itLumis->second))
+					return true;
+		return false;
+	}
+private:
+	run_id passRun;
+	std::map<run_id, std::vector<std::pair<lumi_id, lumi_id> > > lumifilter;
+};
