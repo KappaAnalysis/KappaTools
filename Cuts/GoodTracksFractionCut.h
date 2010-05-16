d31694e00488e9c92ad2feda709cbd8a3bbd08e5
Manuel Zeise <zeise@cern.ch>
2010-05-16 18:04:38 +0200

    adding TrackSummary to GoodTracksFractionCut

--- Cuts/GoodTracksFractionCut.h
+++ Cuts/GoodTracksFractionCut.h
@@ -10,17 +10,22 @@ namespace KappaTools
 	class GoodTracksFractionCut : public BaseCut
 	{
 		private:
-			KDataTracks * obj;
+			KDataTracks * obj_Tracks;
+			KTrackSummary * obj_TrackSummary;
 			double min;
 			double max;
+			int numtracks;
 			unsigned int goodTrackMask;
 		public:
-			GoodTracksFractionCut() : BaseCut("good track ratio"), obj(0), min(0.), max(1.), goodTrackMask(0) {};
-			GoodTracksFractionCut(KDataTracks * tmpObj) : BaseCut("good track ratio"), obj(tmpObj), min(0.), max(1.), goodTrackMask(0) {};
+			GoodTracksFractionCut() : BaseCut("good track ratio"), obj_Tracks(0), obj_TrackSummary(0), min(0.), max(1.), numtracks(10), goodTrackMask(1 << 2) {};
+			GoodTracksFractionCut(KDataTracks * tmpObj) : BaseCut("good track ratio"), obj_Tracks(tmpObj), obj_TrackSummary(0), min(0.), max(1.), numtracks(10), goodTrackMask(1 << 2) {};
+			GoodTracksFractionCut(KTrackSummary * tmpObj) : BaseCut("good track ratio"), obj_Tracks(0), obj_TrackSummary(tmpObj), min(0.), max(1.), numtracks(10), goodTrackMask(1 << 2) {};
 		
 			void setPointer(KDataTracks * tmpObj);
+			void setPointer(KTrackSummary * tmpObj);
 		
 			void setGoodTrackMask(unsigned int tmp_);
+			void setNumTracks(int numtracks_);
 			void setMinCut(double min_);
 			void setMaxCut(double max_);
 		
