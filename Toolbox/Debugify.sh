50d1e25d9d95f2e2a855e0e3a436c86edece141a
Fred Stober <stober@cern.ch>
2010-05-17 19:05:29 +0200

    ws was here

--- Toolbox/Debugify.sh
+++ Toolbox/Debugify.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 [ ! -f "$2" ] && echo "Syntax: <on/off> <FILE>" && exit
+
 if [ "$1" == "on" ]; then
-sed -i -e "s@{@{DEBUGMSG();@" $2
-sed -i -e "s@}@DEBUGMSG();}@" $2
+	sed -i -e "s@{@{DEBUGMSG();@" $2
+	sed -i -e "s@}@DEBUGMSG();}@" $2
 else
-sed -i -e "s@{DEBUGMSG();@{@" $2
-sed -i -e "s@DEBUGMSG();}@}@" $2
+	sed -i -e "s@{DEBUGMSG();@{@" $2
+	sed -i -e "s@DEBUGMSG();}@}@" $2
 fi
