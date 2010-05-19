b47a6c07b58dca8c6b87b802eed5a47cdb05095b
Fred Stober <stober@cern.ch>
2010-05-19 05:16:13 +0200

    get rid of __Xstatic, fix in makefile

--- Toolbox/StringTools.cpp
+++ Toolbox/StringTools.cpp
@@ -55,3 +55,34 @@ vector<string> tokenize(const string &str, const string &delim, const bool escap
 		}
 	return result;
 }
+
+template <>
+std::string str<bool>(const bool &i)
+{
+	if (i)
+		return "true";
+	return "false";
+}
+
+template <>
+std::string parse<std::string>(const std::string &s, bool)
+{
+	return s;
+}
+
+template <>
+bool parse<bool>(const std::string &s, bool)
+{
+	static const std::string s_true[] = {"true", "yes", "y", "1"};
+	static const std::string s_false[] = {"false", "no", "n", "0"};
+
+	const std::string tmp = tolower(s);
+	for (size_t i = 0; i < sizeof(s_true) / sizeof(std::string); ++i)
+		if (s_true[i] == tmp)
+			return true;
+	for (size_t i = 0; i < sizeof(s_false) / sizeof(std::string); ++i)
+		if (s_false[i] == tmp)
+			return false;
+	std::cerr << "Parse error - invalid bool: " << s;
+	return false;
+}
