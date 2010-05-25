86da4c45d04f0b8034bdab001c548205ec90d542
Fred Stober <stober@cern.ch>
2010-05-26 00:16:16 +0200

    Added function to get list of branches

--- RootTools/Directory.cpp
+++ RootTools/Directory.cpp
@@ -13,3 +13,21 @@ std::vector<std::string> DirObjects(const TDirectory *dir, std::string filter)
 	}
 	return objects;
 }
+
+std::vector<std::string> TreeObjects(TTree &chain, const std::string cname, const bool inherited)
+{
+	std::vector<std::string> result;
+	TObjArray *branches = chain.GetListOfBranches();
+	if (branches == 0)
+		return result;
+	TClass *req = TClass::GetClass(cname.c_str());
+	std::string reqName = req->GetName();
+	for (int i = 0; i < branches->GetEntries(); ++i)
+	{
+		TBranch *b = (TBranch*)branches->At(i);
+		TClass *cur = TClass::GetClass(b->GetClassName());
+		if ((cur == req) || (inherited && cur->InheritsFrom(req)))
+			result.push_back(b->GetName());
+	}
+	return result;
+}
