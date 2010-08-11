41b172405e12eb9e84edd71aa9c64a0a9e87d801
Andreas Oehler <andreasoehler@gmx.net>
2010-08-11 15:35:49 +0200

    now add some help

--- Tools/duplicateEventChecker.cc
+++ Tools/duplicateEventChecker.cc
@@ -14,50 +14,62 @@ std::ostream& operator <<(std::ostream& output, const EventId& id){
 
 set<EventId> _globalEventCheckerSet;
 
+string progname;
+
+void MyHelpFunction(string arg){
+  cout<<"Duplicate Event Checker Help: "<<endl;
+  cout<<progname<<" [list of filenames] "<<endl;
+  exit(1);
+}
 
 int main(int argc, char* argv[])
 {
-	std::vector<std::string> filenames = CmdLineBase::ParseArgs(argc, argv, OPT_Help | OPT_Version);
-	
-	cout<<"Reading files: "<<endl;
-	for (std::vector<std::string>::iterator it=filenames.begin(); it!=filenames.end();it++)
-	  cout<<*it<<", ";
+  static CmdLineOptionCallback clVersion('h', "help", "Print help", MyHelpFunction);
+  progname=argv[0];
+  std::vector<std::string> filenames = CmdLineBase::ParseArgs(argc, argv);
 
-	cout<<endl;
-
-	cout<<"\nDuplicate Event Checker "<<endl;
-	cout<<"-- currently only checking for duplicate run/lumisection/event numbers."<<endl;
-	cout<<"Feel free to add physical event-checking for Generator etc. Yourself, as this needs to be adopted to the specific sample. (Check Muon/Jet Pt, etc.)\n"<<endl;
-	
-	FileInterface kpFi(filenames,false,2);
-	TChain *theChain=&kpFi.eventdata;
-	KEventMetadata *metadata;
-	KGenEventMetadata *kGen;
-	kpFi.AssignEventPtr(&metadata,&kGen);
-	run_id rid(0);
-	lumi_id lid(0);
-	event_id nCmsEvent(0);
-	//now code the super-sophisticated event loop.
-	const unsigned nEvents=theChain->GetEntries();
-	_globalEventCheckerSet.clear();
-	ProgressMonitor pm(nEvents);
-	
-	for (unsigned i=0;i<nEvents;++i){
-	  if (!pm.Update()) break;
-	  theChain->GetEntry(i);
-	  nCmsEvent=metadata->nEvent;
-	  rid=metadata->nRun;
-	  lid=metadata->nLumi;
-	  EventId tmpId(rid,lid,nCmsEvent);
-	  if (_globalEventCheckerSet.find(tmpId)!=_globalEventCheckerSet.end()){
-	    cerr<<"Duplicate Event found!!! "<<endl;
-	    cout<<tmpId<<endl;
-	    exit(1);
-	  }
-	  else _globalEventCheckerSet.insert(tmpId);
-	}
-	
-	cout<<"RESULT: No duplicate events found."<<endl;
-	return 0;
+  if (filenames.size()==0) {
+    cerr<<"No input files given!"<<endl;
+    MyHelpFunction("");
+  }
+  cout<<"Reading files: "<<endl;
+  for (std::vector<std::string>::iterator it=filenames.begin(); it!=filenames.end();it++)
+    cout<<*it<<", ";
+  
+  cout<<endl;
+  
+  cout<<"\nDuplicate Event Checker "<<endl;
+  cout<<"-- currently only checking for duplicate run/lumisection/event numbers."<<endl;
+  cout<<"Feel free to add physical event-checking for Generator etc. Yourself, as this needs to be adopted to the specific sample. (Check Muon/Jet Pt, etc.)\n"<<endl;
+  
+  FileInterface kpFi(filenames,false,2);
+  TChain *theChain=&kpFi.eventdata;
+  KEventMetadata *metadata;
+  KGenEventMetadata *kGen;
+  kpFi.AssignEventPtr(&metadata,&kGen);
+  run_id rid(0);  lumi_id lid(0);
+  event_id nCmsEvent(0);
+  //now code the super-sophisticated event loop.
+  const unsigned nEvents=theChain->GetEntries();
+  _globalEventCheckerSet.clear();
+  ProgressMonitor pm(nEvents);
+  
+  for (unsigned i=0;i<nEvents;++i){
+    if (!pm.Update()) break;
+    theChain->GetEntry(i);
+    nCmsEvent=metadata->nEvent;
+    rid=metadata->nRun;
+    lid=metadata->nLumi;
+    EventId tmpId(rid,lid,nCmsEvent);
+    if (_globalEventCheckerSet.find(tmpId)!=_globalEventCheckerSet.end()){
+      cerr<<"Duplicate Event found!!! "<<endl;
+      cout<<tmpId<<endl;
+      exit(1);
+    }
+    else _globalEventCheckerSet.insert(tmpId);
+  }
+  
+  cout<<"RESULT: No duplicate events found."<<endl;
+  return 0;
 }
 
