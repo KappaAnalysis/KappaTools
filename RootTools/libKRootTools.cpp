b0493784bef95301545313639be6f2e06da081e2
Fred Stober <stober@cern.ch>
2010-10-05 23:45:45 +0200

    added base lib file

---
+++ RootTools/libKRootTools.cpp
@@ -0,0 +1,4 @@
+#include "libKRootTools.h"
+#include "Toolbox/Version.h"
+
+static VersionInfo versionKTB("Kappa Root Tools", QUOTE(VCS_REVISION));
---
+++ RootTools/libKRootTools.h
@@ -0,0 +1,15 @@
+#ifndef ROOTTOOLS_H
+#define ROOTTOOLS_H
+
+#include "Directory.h"
+#include "DisplayTools.h"
+#include "EventLogger.h"
+#include "FileInterface.h"
+//#include "JECTools.h"
+#include "JetID.h"
+#include "LumiMetadataProvider.h"
+#include "Matching.h"
+#include "RunLumiReader.h"
+#include "SortTools.h"
+
+#endif
