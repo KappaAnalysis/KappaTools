9c1703e62eea1d38a73585fd0a7aa4e75b10dbf1
Fred Stober <stober@cern.ch>
2010-10-05 23:40:28 +0200

    updated displaytools

--- RootTools/DisplayTools.h
+++ RootTools/DisplayTools.h
@@ -16,7 +16,7 @@ void displayLVs(const std::vector<T> &vec, size_t maxN = 0)
 	else
 		maxN = vec.size();
 	for (size_t j = 0; j < maxN; ++j)
-		std::cout << KLVWrap<typename T::KInternalLV>(vec[j].p4) << std::endl;
+		std::cout << vec[j].p4 << std::endl;
 }
 
 template<typename T>
@@ -28,4 +28,20 @@ void displayJets(const std::vector<T> &vec, size_t maxN = 0)
 		maxN = vec.size();
 	for (size_t j = 0; j < maxN; ++j)
 		std::cout << vec[j] << std::endl;
+}
+
+template<typename T1, typename T2>
+void displayMatching(const T1 *baseObj, const T2 *targetObj, const std::vector<int> &match)
+{
+	for (size_t j = 0; j < match.size(); ++j)
+	{
+		const int k = match[j];
+		std::cout << j << "->" << k << "\t";
+		if (k < 0)
+		{
+			std::cout << std::endl;
+			continue;
+		}
+		std::cout << targetObj->at(j).p4 << " -> " << baseObj->at(k).p4 << std::endl;
+	}
 }
