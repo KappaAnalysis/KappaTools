8e7b0d74d91de00b56acf51f56c4ae266ab31c98
Fred Stober <stober@cern.ch>
2010-10-27 22:07:03 +0200

    Small fixes

--- Cuts/CutFlow.h
+++ Cuts/CutFlow.h
@@ -45,6 +45,7 @@ namespace KappaTools
 
 		CutFlow();
 		CutFlow(CutFlow * cf);
+		~CutFlow() {};
 	};
 
 	class CutflowTable
--- RootTools/RunLumiReader.cpp
+++ RootTools/RunLumiReader.cpp
@@ -1,8 +1,8 @@
 #include "RunLumiReader.h"
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
-#include "Toolbox/StringTools.h"
 #include "Toolbox/IOHelper.h"
+#include "Toolbox/StringTools.h"
 
 void readLumiFilter(const std::string json, std::map<run_id, std::set<std::pair<lumi_id, lumi_id> > > &lumifilter)
 {
@@ -99,4 +99,3 @@ std::ostream &operator<<(std::ostream &os, RunLumiSelector &m)
 	}
 	return os;
 }
-
