8e9811f28e34287f2d1560068297d30d288f3572
Fred Stober <stober@cern.ch>
2011-05-25 18:21:38 +0200

    Added xsec tool

---
+++ Tools/xsec.cpp
@@ -0,0 +1,32 @@
+#include "DataFormats/interface/KMetadata.h"
+#include "KappaTools/RootTools/RunLumiReader.h"
+#include "KappaTools/RootTools/FileInterface.h"
+#include "Toolbox/Version.h"
+#include "Toolbox/CmdLineSetup.h"
+
+using namespace std;
+
+static VersionInfo version("Average xsec calculator", QUOTE(SVN_REV));
+
+int main(int argc, char **argv)
+{
+	vector<string> files = CmdLineBase::ParseArgs(argc, argv, OPT_Help | OPT_Version);
+
+	// Loading files
+	FileInterface fi(files, false, 0);
+
+	double xsec = 0;
+	vector<pair<run_id, lumi_id> > lumis = fi.GetRunLumis();
+	for (size_t l = 0; l < lumis.size(); ++l)
+	{
+		run_id run = lumis[l].first;
+		lumi_id lumi = lumis[l].second;
+		xsec += fi.GetGenLumiMetadata(run, lumi)->xSectionInt;
+	}
+	cout << "Average xsec: " << xsec / lumis.size() << endl;
+
+	const int nEvents = fi.eventdata.GetEntries();
+	cout << "Number of events: " << nEvents << endl << endl;
+
+	return 0;
+}
