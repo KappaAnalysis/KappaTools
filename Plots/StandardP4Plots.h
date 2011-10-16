f82800b663418a2e0804408a2530a90c47672f18
Manuel Zeise <zeise@cern.ch>
2011-10-16 21:28:14 +0200

    move PlotsByEta and PlotsByPt into separate file to simplify dependencies

--- Plots/StandardP4Plots.h
+++ Plots/StandardP4Plots.h
@@ -2,13 +2,10 @@
 #define KAPPA_STANDARDPLOTS_H
 
 #include "TH1D.h"
+#include "TH2D.h"
 #include "TDirectory.h"
 #include "BasePlot.h"
 #include "Kappa/DataFormats/interface/KLorentzVector.h"
-
-#include "../Objects/Zmumu.h"
-#include "MuonPlots.h"
-#include "ZmumuPlots.h"
 
 namespace KappaTools
 {
@@ -40,55 +37,6 @@ namespace KappaTools
 		void process(RMLV p4, double weight = 1.0);
 		void process(RMDataLV p4, double weight = 1.0);
 		void final();
-	};
-
-	// ObjectType1 -- Object to plot
-	// ObjectType2 -- Object for binning
-	template <typename PlottingClass, typename ObjectType1, typename ObjectType2>
-	class PlotsByPt : public BasePlot
-	{
-		private:
-			PlottingClass * all, * underflow, * overflow;
-			std::vector<PlottingClass> plotsByBin;
-			std::vector<double> binning;
-			int mode;
-		public:
-			//PlotsByPt(TDirectory * tmpFile, TString tmpDirectory="");
-			PlotsByPt(std::vector<double> binning_, TDirectory * tmpFile, TString tmpDirectory="");
-			void process(ObjectType1 * obj, KDataVertex * pv = 0, double weight = 1., ObjectType2 * obj2 = 0);
-			void final();
-
-			void setMode(char mode_);
-
-			static const char FILL_EXCL = 1;
-			static const char FILL_INCL_UP = 2;
-			static const char FILL_INCL_DOWN = 4;
-
-			static const char FILL_ABS = 8;
-	};
-
-	// ObjectType1 -- Object to plot
-	// ObjectType2 -- Object for binning
-	template <typename PlottingClass, typename ObjectType1, typename ObjectType2>
-	class PlotsByEta : public BasePlot
-	{
-		private:
-			PlottingClass * all, * underflow, * overflow;
-			std::vector<PlottingClass> plotsByBin;
-			std::vector<double> binning;
-			int mode;
-		public:
-			PlotsByEta(std::vector<double> binning_, TDirectory * tmpFile, TString tmpDirectory="");
-			void process(ObjectType1 * obj, KDataVertex * pv = 0, double weight = 1., ObjectType2 * obj2 = 0);
-			void final();
-
-			void setMode(char mode_);
-
-			static const char FILL_EXCL = 1;
-			static const char FILL_INCL_UP = 2;
-			static const char FILL_INCL_DOWN = 4;
-
-			static const char FILL_ABS = 8;
 	};
 }
 #endif
