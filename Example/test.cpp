f8cb4af6e752d17d7dd59d7329ef014d947db891
Fred Stober <stober@cern.ch>
2011-11-22 14:28:40 +0100

    updated example

--- Example/test.cpp
+++ Example/test.cpp
@@ -7,19 +7,27 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+	std::cout << "Loading files..." << std::endl;
+	std::cout.flush();
 	FileInterface fi(vector<string>(1, argv[1]));
-
+	std::cout << "finished" << std::endl;
 	vector<string> names = fi.GetNames<KDataLVs>();
 	cout << names << endl << endl;
 	if (names.size() == 0)
 		return -1;
+
 	KDataLVs *jets = fi.Get<KDataLVs>(names[0]);
 	KDataBeamSpot *bs = fi.Get<KDataBeamSpot>("offlineBeamSpot");
+
+	std::map<std::string, KDataLVs*> tomap;
+	for (int i = 0; i < names.size(); ++i)
+		tomap[names[i]] = fi.Get<KDataLVs>(names[i]);
 
 	KEventMetadata *meta_event = fi.Get<KEventMetadata>();
 //	KGenEventMetadata *meta_event_gen = fi.Get<KGenEventMetadata>();
 
-	long long nEvents = min((long long)3, fi.eventdata.GetEntries());
+//	long long nEvents = min((long long)3, fi.eventdata.GetEntries());
+	long long nEvents = fi.eventdata.GetEntries();
 	ProgressMonitor pm(nEvents);
 	for (long long iEvent = 0; iEvent < nEvents; ++iEvent)
 	{
@@ -32,6 +40,9 @@ int main(int argc, char **argv)
 		KLumiMetadata *meta_lumi = fi.Get<KLumiMetadata>(meta_event);
 		KGenLumiMetadata *meta_lumi_gen = fi.Get<KGenLumiMetadata>(meta_event);
 
+		if (meta_event->bitsHLT == 0)
+			continue;
+
 		cout << "Event metadata: " << *meta_event << endl;
 		if (meta_lumi_gen)
 			cout << "Generator lumi metadata: " << *meta_lumi_gen;
@@ -40,6 +51,18 @@ int main(int argc, char **argv)
 		cout << "Jets: " << jets->size() << " => " << *jets << endl;
 		if (bs)
 			cout << "Beamspot: " << *bs << endl << endl;
+		for (int i = 0; i < names.size(); ++i)
+		{
+			if (tomap[names[i]]->size() > 0)
+			{
+				cout << names[i] << " " << tomap[names[i]]->size() << endl;
+				cout << *tomap[names[i]] << endl;
+			}
+		}
+		for (int i = 0; i < meta_lumi->hltNames.size(); ++i)
+			if (meta_event->bitsHLT & (1ul << i))
+				cout << meta_lumi->hltNames[i] << " ";
+		cout << endl;
 		cout << "=========" << endl << endl;
 	}
 
