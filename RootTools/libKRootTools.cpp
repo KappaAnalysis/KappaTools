199233e8ec421261c72df2401bc4250f12cfa90a
Fred Stober <stober@cern.ch>
2011-11-22 14:28:11 +0100

    Added FileIntefaceBase to makefile Fixed symbol collision

--- RootTools/libKRootTools.cpp
+++ RootTools/libKRootTools.cpp
@@ -1,3 +1,3 @@
 #include "../Toolbox/libKToolbox.h"
 
-static VersionInfo versionKTB("Kappa Root Tools", QUOTE(VCS_REVISION));
+static VersionInfo versionKRB("Kappa Root Tools", QUOTE(VCS_REVISION));
