e540d349f566bae47af6c77b2225064a6509d2c0
Thomas Mueller <tmuller@cern.ch>
2015-11-30 10:57:01 +0100

    ini_KappaTools.sh: remove Plots

--- Toolbox/scripts/ini_KappaTools.sh
+++ Toolbox/scripts/ini_KappaTools.sh
@@ -8,7 +8,7 @@ else
 	export KAPPATOOLSPATH=$(readlink -f $(dirname $(readlink -f ${BASH_SOURCE[0]}))/../..)
 fi
 
-export SCRAM_IGNORE_PACKAGES="KappaTools/Plots KappaTools/PythonTools"
+export SCRAM_IGNORE_PACKAGES="KappaTools/PythonTools"
 
 export KAPPAPATH=$(readlink -f $KAPPATOOLSPATH/../Kappa)
 
