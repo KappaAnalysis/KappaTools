a9e823cd1b254ba7fe7648f40c4177eec7c56ecd
Thomas Mueller <t.muller@cern.ch>
2016-12-19 14:40:01 +0100

    HLTTools: move code to cpp file

--- RootTools/interface/HLTTools.h
+++ RootTools/interface/HLTTools.h
@@ -1,12 +1,8 @@
-/* Copyright (c) 2011 - All Rights Reserved
- *   Manuel Zeise <zeise@cern.ch>
- */
 
 #ifndef KAPPA_HLTTOOLS_H
 #define KAPPA_HLTTOOLS_H
 
 #include <Kappa/DataFormats/interface/Kappa.h>
-#include <boost/regex.hpp>
 
 class HLTTools
 {
@@ -22,88 +18,14 @@ private:
 	
 	
 public:
-	HLTTools(KLumiInfo * lumiInfo = nullptr)
-	{
-	  akt_nLumi = 0; // just a default value, will be overwritten by first lumi
-	  akt_nRun = 0;// just a default value, will be overwritten by first lumi
-	  refill_hlt = true; // Allways fill the hlt infos in the first run
-	  if (lumiInfo){
-	    setLumiInfo(lumiInfo);
-	  }
-	}
-	void setLumiInfo(KLumiInfo * lumiInfo)
-	{
-		this->lumiInfo = lumiInfo;		
-		if (lumiInfo->nLumi != akt_nLumi || lumiInfo->nRun != akt_nRun){ // check if lumi or run number has changed
-		  akt_nLumi = lumiInfo->nLumi; // save new lumi
-		  akt_nRun  = lumiInfo->nRun; // save new akt run
-		  refill_hlt = true; // tirgger new refill
-		}
-		
-		if (refill_hlt){
-		  nameCache.clear();
-		  posCache.clear();
-		  for (size_t idx = 0; idx < lumiInfo->hltNames.size(); ++idx){
-		    posCache[lumiInfo->hltNames[idx]] = idx;
-		    nameCache[lumiInfo->hltNames[idx]] = lumiInfo->hltNames[idx];
-		  }
-		  nameCacheEndIt = nameCache.end();
-		  refill_hlt = false;	
-		}
-	}
-	bool isHLTDefect() const
-	{
-		if (!lumiInfo)
-			return true;
-		for (size_t idx = 1; idx < lumiInfo->hltNames.size(); idx++)
-			if (!lumiInfo->hltPrescales[idx])
-				return true;
-		return false;
-	}
-	bool isPrescaled(const std::string &hltName) const
-	{
-		return (getPrescale(hltName) > 1);
-	}
-	bool isAvailable(const std::string &hltName) const
-	{
-		return (getHLTName(hltName) != "");
-	}
-	unsigned int getPrescale(const std::string &hltName) const
-	{
-		if (!lumiInfo)
-			return 0;
-		std::string tmpHLTName = getHLTName(hltName);
-		if (tmpHLTName == "")
-			return 0;
-		return lumiInfo->hltPrescales[posCache[tmpHLTName]];
-	}
-	std::string getHLTName(const std::string &hltName) const
-	{
-		itCache = nameCache.find(hltName);
-		if (itCache != nameCacheEndIt)
-			return itCache->second;
-
-		if (!lumiInfo)
-			return "";
-
-		std::vector<std::string>::const_iterator endIt = lumiInfo->hltNames.end();
-		boost::regex pattern(hltName+"(_v[[:digit:]]+)?$", boost::regex::icase | boost::regex::extended);
-		for (std::vector<std::string>::const_iterator curIt = lumiInfo->hltNames.begin(); curIt != endIt; ++curIt)
-			if (boost::regex_search(*curIt, pattern))
-			{
-				nameCache[hltName] = *curIt;
-				nameCacheEndIt = nameCache.end();
-				return *curIt;
-			}
-		return "";
-	}
-	size_t getHLTPosition(const std::string &hltName) const
-	{
-		std::string tmpHLTName = getHLTName(hltName);
-		if (tmpHLTName == "")
-			return 0;
-		return posCache[tmpHLTName];
-	}
+	HLTTools(KLumiInfo * lumiInfo = nullptr);
+	void setLumiInfo(KLumiInfo * lumiInfo);
+	bool isHLTDefect() const;
+	bool isPrescaled(const std::string &hltName) const;
+	bool isAvailable(const std::string &hltName) const;
+	unsigned int getPrescale(const std::string &hltName) const;
+	std::string getHLTName(const std::string &hltName) const;
+	size_t getHLTPosition(const std::string &hltName) const;
 
 };
 
--- RootTools/src/HLTTools.cpp
+++ RootTools/src/HLTTools.cpp
@@ -1,5 +1,89 @@
-/* Copyright (c) 2011 - All Rights Reserved
- *   Manuel Zeise <zeise@cern.ch>
- */
 
 #include "../interface/HLTTools.h"
+
+#include <boost/regex.hpp>
+
+
+HLTTools::HLTTools(KLumiInfo * lumiInfo)
+{
+	akt_nLumi = 0; // just a default value, will be overwritten by first lumi
+	akt_nRun = 0;// just a default value, will be overwritten by first lumi
+	refill_hlt = true; // Allways fill the hlt infos in the first run
+	if (lumiInfo){
+		setLumiInfo(lumiInfo);
+	}
+}
+void HLTTools::setLumiInfo(KLumiInfo * lumiInfo)
+{
+	this->lumiInfo = lumiInfo;
+	if (lumiInfo->nLumi != akt_nLumi || lumiInfo->nRun != akt_nRun){ // check if lumi or run number has changed
+		akt_nLumi = lumiInfo->nLumi; // save new lumi
+		akt_nRun = lumiInfo->nRun; // save new akt run
+		refill_hlt = true; // tirgger new refill
+	}
+	
+	if (refill_hlt){
+		nameCache.clear();
+		posCache.clear();
+		for (size_t idx = 0; idx < lumiInfo->hltNames.size(); ++idx){
+			posCache[lumiInfo->hltNames[idx]] = idx;
+			nameCache[lumiInfo->hltNames[idx]] = lumiInfo->hltNames[idx];
+		}
+		nameCacheEndIt = nameCache.end();
+		refill_hlt = false;
+	}
+}
+bool HLTTools::isHLTDefect() const
+{
+	if (!lumiInfo)
+		return true;
+	for (size_t idx = 1; idx < lumiInfo->hltNames.size(); idx++)
+		if (!lumiInfo->hltPrescales[idx])
+			return true;
+	return false;
+}
+bool HLTTools::isPrescaled(const std::string &hltName) const
+{
+	return (getPrescale(hltName) > 1);
+}
+bool HLTTools::isAvailable(const std::string &hltName) const
+{
+	return (getHLTName(hltName) != "");
+}
+unsigned int HLTTools::getPrescale(const std::string &hltName) const
+{
+	if (!lumiInfo)
+		return 0;
+	std::string tmpHLTName = getHLTName(hltName);
+	if (tmpHLTName == "")
+		return 0;
+	return lumiInfo->hltPrescales[posCache[tmpHLTName]];
+}
+std::string HLTTools::getHLTName(const std::string &hltName) const
+{
+	itCache = nameCache.find(hltName);
+	if (itCache != nameCacheEndIt)
+		return itCache->second;
+
+	if (!lumiInfo)
+		return "";
+
+	std::vector<std::string>::const_iterator endIt = lumiInfo->hltNames.end();
+	boost::regex pattern(hltName+"(_v[[:digit:]]+)?$", boost::regex::icase | boost::regex::extended);
+	for (std::vector<std::string>::const_iterator curIt = lumiInfo->hltNames.begin(); curIt != endIt; ++curIt)
+		if (boost::regex_search(*curIt, pattern))
+		{
+			nameCache[hltName] = *curIt;
+			nameCacheEndIt = nameCache.end();
+			return *curIt;
+		}
+	return "";
+}
+size_t HLTTools::getHLTPosition(const std::string &hltName) const
+{
+	std::string tmpHLTName = getHLTName(hltName);
+	if (tmpHLTName == "")
+		return 0;
+	return posCache[tmpHLTName];
+}
+
